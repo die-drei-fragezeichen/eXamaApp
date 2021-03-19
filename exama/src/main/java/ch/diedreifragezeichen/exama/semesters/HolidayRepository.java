9fd512aa04b0c0bd75e80611f2f88c88d58395a1
rotheman <roman.pfaeffli@unifr.ch>
2021-03-19 22:51:22 +0100

    holiday update - old problem

--- exama/src/main/java/ch/diedreifragezeichen/exama/semesters/HolidayRepository.java
+++ exama/src/main/java/ch/diedreifragezeichen/exama/semesters/HolidayRepository.java
@@ -3,5 +3,5 @@ package ch.diedreifragezeichen.exama.semesters;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HolidayRepository extends JpaRepository<Holiday, Long> {
-    public Semester findHolidayById(Long id);
+    public Holiday findHolidayById(Long id);
 }
