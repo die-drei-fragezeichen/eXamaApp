ec3827c7054664be3400632c2b301d0be829d372
rotheman <roman.pfaeffli@unifr.ch>
2021-03-19 21:42:22 +0100

    update sesson - em working

--- exama/src/main/java/ch/diedreifragezeichen/exama/courses/CoreCourseService.java
+++ exama/src/main/java/ch/diedreifragezeichen/exama/courses/CoreCourseService.java
@@ -3,19 +3,18 @@ package ch.diedreifragezeichen.exama.courses;
 import javax.persistence.*;
 import javax.transaction.Transactional;
 
-import org.hibernate.Session;
-
 import org.springframework.stereotype.Service;
 
 @Service("CoreCourseService")
-public class CoreCourseService{
+public class CoreCourseService {
 
     @PersistenceContext
     private EntityManager em;
 
     @Transactional
-    public void saveOrUpdateCoreCourse(CoreCourse coreCourse){
-        Session session = em.unwrap(org.hibernate.Session.class);
-        session.saveOrUpdate(coreCourse);
+    public void saveOrUpdateCoreCourse(CoreCourse coreCourse) {
+        em.unwrap(org.hibernate.Session.class).saveOrUpdate(coreCourse);
+        // .unwrap provides the current Session via JPA, this session then successfully
+        // perform update or new entity when called.
     }
 }
