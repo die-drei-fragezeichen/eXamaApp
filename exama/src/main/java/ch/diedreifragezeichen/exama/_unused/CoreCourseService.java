0c96a3fbaa8fedcf62e43d69b62bcf8b8d6bbaec
rotheman <roman.pfaeffli@unifr.ch>
2021-03-20 17:56:08 +0100

    sry löite 2

--- exama/src/main/java/ch/diedreifragezeichen/exama/_unused/CoreCourseService.java
+++ exama/src/main/java/ch/diedreifragezeichen/exama/_unused/CoreCourseService.java
@@ -1,23 +1,23 @@
-package ch.diedreifragezeichen.exama._unused;
+// package ch.diedreifragezeichen.exama._unused;
 
-import javax.persistence.*;
-import javax.transaction.Transactional;
+// import javax.persistence.*;
+// import javax.transaction.Transactional;
 
-import org.springframework.stereotype.Service;
+// import org.springframework.stereotype.Service;
 
-import ch.diedreifragezeichen.exama.courses.CoreCourse;
+// import ch.diedreifragezeichen.exama.courses.CoreCourse;
 
-@Service("CoreCourseService")
-public class CoreCourseService {
+// @Service("CoreCourseService")
+// public class CoreCourseService {
 
-    @PersistenceContext
-    private EntityManager em;
+//     @PersistenceContext
+//     private EntityManager em;
 
-    @Transactional
-    public void saveOrUpdateCoreCourse(CoreCourse coreCourse) {
-        em.unwrap(org.hibernate.Session.class).saveOrUpdate(coreCourse);
-        // .unwrap provides the current Session via JPA, as provided by the
-        // entityManager. this session then successfully
-        // perform update or new entity when called.
-    }
-}
+//     @Transactional
+//     public void saveOrUpdateCoreCourse(CoreCourse coreCourse) {
+//         em.unwrap(org.hibernate.Session.class).saveOrUpdate(coreCourse);
+//         // .unwrap provides the current Session via JPA, as provided by the
+//         // entityManager. this session then successfully
+//         // perform update or new entity when called.
+//     }
+// }
--- exama/src/main/java/ch/diedreifragezeichen/exama/_unused/WorkloadController.java
+++ exama/src/main/java/ch/diedreifragezeichen/exama/_unused/WorkloadController.java
@@ -1,48 +1,48 @@
-package ch.diedreifragezeichen.exama._unused;
+// package ch.diedreifragezeichen.exama._unused;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.servlet.ModelAndView;
 
-import ch.diedreifragezeichen.exama.assignments.workload.Workload;
-import ch.diedreifragezeichen.exama.assignments.workload.WorkloadDistribution;
-import ch.diedreifragezeichen.exama.assignments.workload.WorkloadDistributionRepository;
-import ch.diedreifragezeichen.exama.assignments.workload.WorkloadRepository;
+// import ch.diedreifragezeichen.exama.assignments.workload.Workload;
+// import ch.diedreifragezeichen.exama.assignments.workload.WorkloadDistribution;
+// import ch.diedreifragezeichen.exama.assignments.workload.WorkloadDistributionRepository;
+// import ch.diedreifragezeichen.exama.assignments.workload.WorkloadRepository;
 
-@Controller
-public class WorkloadController {
+// @Controller
+// public class WorkloadController {
 
-    @Autowired
-    private WorkloadRepository workloadRepo;
-    @Autowired
-    private WorkloadDistributionRepository distributionRepo;
+//     @Autowired
+//     private WorkloadRepository workloadRepo;
+//     @Autowired
+//     private WorkloadDistributionRepository distributionRepo;
 
-    @GetMapping("/workloads/show")
-    public String showWorkloads(Model model) {
-        List<Workload> listWorkloads = workloadRepo.findAll();
-        model.addAttribute("liste", listWorkloads);
-        return "adminTemplates/workloadsShow";
-    }
+//     @GetMapping("/workloads/show")
+//     public String showWorkloads(Model model) {
+//         List<Workload> listWorkloads = workloadRepo.findAll();
+//         model.addAttribute("liste", listWorkloads);
+//         return "adminTemplates/workloadsShow";
+//     }
 
-    @GetMapping("/workloads/create")
-    public ModelAndView newWorkload() {
-        Workload workload = new Workload();
-        ModelAndView mav = new ModelAndView("adminTemplates/workloadCreate");
-        mav.addObject("newWorkload", workload);
-        List<WorkloadDistribution> listDist = distributionRepo.findAll();
-        mav.addObject("allDist", listDist);
-        return mav;
-    }
+//     @GetMapping("/workloads/create")
+//     public ModelAndView newWorkload() {
+//         Workload workload = new Workload();
+//         ModelAndView mav = new ModelAndView("adminTemplates/workloadCreate");
+//         mav.addObject("newWorkload", workload);
+//         List<WorkloadDistribution> listDist = distributionRepo.findAll();
+//         mav.addObject("allDist", listDist);
+//         return mav;
+//     }
 
-    @PostMapping("/workloads/created")
-    public String processSaving(Workload workload) {
-        workloadRepo.save(workload);
-        return "redirect:/workloads/show";
-    }
+//     @PostMapping("/workloads/created")
+//     public String processSaving(Workload workload) {
+//         workloadRepo.save(workload);
+//         return "redirect:/workloads/show";
+//     }
 
-}
+// }
