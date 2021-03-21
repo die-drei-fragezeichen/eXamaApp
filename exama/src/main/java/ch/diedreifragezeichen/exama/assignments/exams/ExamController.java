f0ac0c146e9993d6c9cef9eee25c457d56361949
rotheman <roman.pfaeffli@unifr.ch>
2021-03-21 03:34:17 +0100

    semesterView updated

--- exama/src/main/java/ch/diedreifragezeichen/exama/assignments/exams/ExamController.java
+++ exama/src/main/java/ch/diedreifragezeichen/exama/assignments/exams/ExamController.java
@@ -1,9 +1,11 @@
 package ch.diedreifragezeichen.exama.assignments.exams;
 
 import java.time.*;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.persistence.*;
 import javax.transaction.Transactional;
@@ -26,6 +28,8 @@ import ch.diedreifragezeichen.exama.courses.Course;
 import ch.diedreifragezeichen.exama.courses.CourseRepository;
 import ch.diedreifragezeichen.exama.operator.Operator;
 import ch.diedreifragezeichen.exama.operator.OperatorRepository;
+import ch.diedreifragezeichen.exama.semesters.Holiday;
+import ch.diedreifragezeichen.exama.semesters.HolidayRepository;
 import ch.diedreifragezeichen.exama.semesters.Semester;
 import ch.diedreifragezeichen.exama.semesters.SemesterRepository;
 import ch.diedreifragezeichen.exama.subjects.Subject;
@@ -63,6 +67,9 @@ public class ExamController {
     @Autowired
     private SemesterRepository semesterRepo;
 
+    @Autowired
+    private HolidayRepository holidayRepo;
+
     @PersistenceContext
     private EntityManager em;
 
@@ -96,7 +103,8 @@ public class ExamController {
         ModelAndView mav = new ModelAndView("teacherTemplates/examModify");
         mav.addObject("exam", exam);
         List<Course> listCourses = courseRepo.findAll();
-        List<Course> usersCourses = listCourses.stream().filter(c -> c.getUsers().contains(user)).collect(Collectors.toList());
+        List<Course> usersCourses = listCourses.stream().filter(c -> c.getUsers().contains(user))
+                .collect(Collectors.toList());
         mav.addObject("allCourses", usersCourses);
         List<ExamType> listTypes = examtypeRepo.findAll();
         mav.addObject("allExamTypes", listTypes);
@@ -119,7 +127,8 @@ public class ExamController {
         Exam exam = examRepo.findExamById(id);
         mav.addObject("exam", exam);
         List<Course> listCourses = courseRepo.findAll();
-        List<Course> usersCourses = listCourses.stream().filter(c -> c.getUsers().contains(user)).collect(Collectors.toList());
+        List<Course> usersCourses = listCourses.stream().filter(c -> c.getUsers().contains(user))
+                .collect(Collectors.toList());
         mav.addObject("allCourses", usersCourses);
         List<ExamType> listTypes = examtypeRepo.findAll();
         mav.addObject("allExamTypes", listTypes);
@@ -292,10 +301,11 @@ public class ExamController {
         }
         mav.addObject("allMondays", mondays);
 
-        // retrieve allExams, week by week, for the whole semester in a list of list of
-        // exams from Monday to Sunday
+        // retrieve allExams, week by week, for the whole semester
+        // dead approach with list of list leads to thymeleaf useability restrictions
+        // dead code: List<List<Exam>> allExams = new ArrayList<>();i
+        // working approach: creating a list of hashmaps --> usage see html.
         monday = semesterStart;
-        // List<List<Exam>> allExams = new ArrayList<>();
         List<HashMap<String, Exam>> allExams = new ArrayList<>();
         while (monday.isBefore(semesterEnd)) {
             List<Exam> examsByWeek = examRepo.findAllByDueDateBetween(monday, monday.with(DayOfWeek.SUNDAY));
@@ -310,14 +320,41 @@ public class ExamController {
         long xExam = allExams.stream().count();
         mav.addObject("xExam", xExam);
 
-        // List<Subject> subjects = allExams.get(0).stream().map(exam ->
-        // exam.getSubject()).collect(Collectors.toList());
+        // create a HolidayHashmap
+        // not used because finding all days between is easier in list: see below
+        monday = semesterStart;
+        List<Holiday> allHolidays = holidayRepo.findAllByStartDateBetween(semesterStart, semesterEnd);
+        mav.addObject("allHolidays", allHolidays);
+        HashMap<LocalDate, Holiday> allHolidaysMap = new HashMap<>();
+        for (Holiday holiday : allHolidays) {
+
+            allHolidaysMap.put(holiday.getStartDate(), holiday);
+        }
+        mav.addObject("allHolidaysMap", allHolidaysMap);
+
+        // create a List containing all the days of all the holidays
+        // this allows catching holidays that are wrongly set, or begin mid-week -> TemporalAdjusters then sets start to following Monday.
+        List<Holiday> allHolidaysList = holidayRepo.findAllByStartDateBetween(semesterStart, semesterEnd);
+        List<LocalDate> allHolidaysDays = new ArrayList<>();
+        for (Holiday holiday : allHolidaysList) {
+            long numOfDays = ChronoUnit.DAYS.between(holiday.getStartDate().with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY)), holiday.getEndDate());
+            List<LocalDate> allDays = Stream.iterate(holiday.getStartDate(), date -> date.plusDays(1))
+                    .limit(numOfDays).collect(Collectors.toList());
+            allHolidaysDays.addAll(allDays);
+        }
+        mav.addObject("allHolidaysList", allHolidaysDays);
+
+
+
+        
 
         List<Subject> allSubjects = subjectRepo.findAll();
         mav.addObject("allSubjects", allSubjects);
         // count all the subjects, method returns "numberOfSubjects"
         calculateNumberOfSubjects(mav, allSubjects);
         return mav;
+
+
 
     }
 
@@ -336,32 +373,4 @@ public class ExamController {
         }
         return sum;
     }
-
-    // @GetMapping("/examView/show")
-    // public String showExamView() {
-    // ModelAndView mav = new ModelAndView("studentTemplates/examTableShow");
-    // List<Subject> allSubjects = subjectRepo.findAll();
-    // mav.addObject("alleFaecher", allSubjects);
-
-    // Datum selectedDatum = operatorRepo.findAll().get(0);
-    // operatorRepo.deleteAll();
-    // LocalDate selectedDate = selectedDatum.getSelectedDate();
-    // model.addAttribute("Datum", selectedDate);
-
-    // ExamService helper = new ExamService();
-    // LocalDate Monday = helper.getFirstDayOfWeek(selectedDate);
-    // model.addAttribute("Montag", Monday);
-
-    // LocalDate Sunday = helper.getLastDayOfWeek(selectedDate);
-    // model.addAttribute("Sonntag", Sunday);
-
-    // List<Exam> listExams = examRepo.findAllByDueDateBetween(Monday, Sunday);
-    // model.addAttribute("liste", listExams);
-
-    // calculateNumberOfExams(model, listExams);
-    // calculateExamFactor(model, listExams);
-
-    // return "studentTemplates/examBarShow";
-
-    // }
 }
--- exama/src/main/java/ch/diedreifragezeichen/exama/semesters/HolidayRepository.java
+++ exama/src/main/java/ch/diedreifragezeichen/exama/semesters/HolidayRepository.java
@@ -1,7 +1,12 @@
 package ch.diedreifragezeichen.exama.semesters;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HolidayRepository extends JpaRepository<Holiday, Long> {
     public Holiday findHolidayById(Long id);
+    public List<Holiday> findAllByStartDateBetween(LocalDate Monday, LocalDate Sunday);
+
 }
