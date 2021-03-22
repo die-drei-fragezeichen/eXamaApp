24815ed89a710bf3f23c9c707e99bca22c20b623
yellow1987 <69144581+yellow1987@users.noreply.github.com>
2021-03-22 15:56:12 +0100

    file added

---
+++ exama/src/main/java/ch/diedreifragezeichen/exama/localizedWeek/MondaySunday.java
@@ -0,0 +1,32 @@
+package ch.diedreifragezeichen.exama.localizedWeek;
+
+import java.time.LocalDate;
+
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.DayOfWeek.TUESDAY;
+import static java.time.DayOfWeek.WEDNESDAY;
+import static java.time.DayOfWeek.THURSDAY;
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.SATURDAY;
+import static java.time.DayOfWeek.SUNDAY;
+import static java.time.temporal.TemporalAdjusters.nextOrSame;
+import static java.time.temporal.TemporalAdjusters.previousOrSame;
+
+public class MondaySunday  {
+    public static void main(String[] args) {
+
+    LocalDate today = LocalDate.now();
+
+    LocalDate monday = today.with(previousOrSame(MONDAY));
+    LocalDate tuesday = monday.with(nextOrSame(TUESDAY));
+    LocalDate wednesday = monday.with(nextOrSame(WEDNESDAY));
+    LocalDate thursday = monday.with(nextOrSame(THURSDAY));
+    LocalDate friday = monday.with(nextOrSame(FRIDAY));
+    LocalDate saturday = monday.with(nextOrSame(SATURDAY));
+    LocalDate sunday = today.with(nextOrSame(SUNDAY));
+    
+    System.out.println("Sunday of the Week: " + sunday);
+    
+  }
+  
+}
