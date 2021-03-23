1234df9be473223da6a7d8b0f1e205d6dadefba0
rotheman <roman.pfaeffli@unifr.ch>
2021-03-23 18:02:30 +0100

    userDetailsEdited

--- exama/src/main/java/ch/diedreifragezeichen/exama/users/UserDetailsExama.java
+++ exama/src/main/java/ch/diedreifragezeichen/exama/users/UserDetailsExama.java
@@ -3,11 +3,16 @@ package ch.diedreifragezeichen.exama.users;
 import java.util.Collection;
 import java.util.Set;
 import java.util.List;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import ch.diedreifragezeichen.exama.assignments.exams.Exam;
+import ch.diedreifragezeichen.exama.courses.CoreCourse;
+import ch.diedreifragezeichen.exama.courses.Course;
 
 @SuppressWarnings("serial")
 public class UserDetailsExama implements UserDetails {
@@ -58,12 +63,58 @@ public class UserDetailsExama implements UserDetails {
         return user.isEnabled();
     }
 
-    public String getFullName() {
-        return user.getFirstName() + " " + user.getLastName();
-    }
+    public Long getId() {
+		return user.getId();
+	}
+
+    public String getEmail() {
+		return user.getEmail();
+	}
 
     public Set<Role> getRoles() {
         return user.getRoles();
     }
+
+    public String getFullName() {
+        return user.getFirstName() + " " + user.getLastName();
+    }
+
+    public String getFirstName() {
+        return user.getFirstName();
+    }
+
+    public String getLastName() {
+        return user.getLastName();
+    }
+
+    public Set<Course> getCourses(){
+        return user.getCourses();
+    }
+
+    public String getAbbreviation() {
+		return user.getAbbreviation();
+	}
+
+    public boolean isLoggedIn() {
+		return user.isLoggedIn();
+	}
+
+    public LocalDate getLastLogin() {
+		return user.getLastLogin();
+	}
+
+    public CoreCourse getCoreCourse() {
+		return user.getCoreCourse();
+	}
+
+    public List<Exam> getExams() {
+		return user.getExams();
+	}
+
+    public List<CoreCourse> getClassTeacherCourses() {
+		return user.getClassTeacherCourses();
+	}
+
+
 
 }
