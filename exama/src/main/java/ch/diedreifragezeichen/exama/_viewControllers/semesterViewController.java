4f566d751ec321b5e060fda9bd1cc190da858820
rotheman <roman.pfaeffli@unifr.ch>
2021-03-27 13:31:35 +0100

    workload hilfsmethoden - unfinished

--- exama/src/main/java/ch/diedreifragezeichen/exama/_viewControllers/semesterViewController.java
+++ exama/src/main/java/ch/diedreifragezeichen/exama/_viewControllers/semesterViewController.java
@@ -16,7 +16,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import ch.diedreifragezeichen.exama.assignments.assignment.Assignment;
 import ch.diedreifragezeichen.exama.assignments.exams.*;
+import ch.diedreifragezeichen.exama.assignments.homeworks.Homework;
 import ch.diedreifragezeichen.exama.courses.CoreCourse;
 import ch.diedreifragezeichen.exama.courses.CoreCourseRepository;
 import ch.diedreifragezeichen.exama.courses.Course;
@@ -219,11 +221,6 @@ public class semesterViewController {
         }
         mav.addObject("allExams", allExams);
 
-        /** Count all exams */
-        // Dead code: keep for practice's sake
-        // long xExam = allExams.stream().count();
-        // mav.addObject("xExam", xExam);
-
         /** create Hashmap mapping every single day of the holiday with holiday */
         monday = semesterStart;
         List<Holiday> listHolidays = holidayRepo.findAllByStartDateBetween(semesterStart, semesterEnd);
@@ -244,6 +241,9 @@ public class semesterViewController {
         mav.addObject("allSubjects", allSubjects);
         // count all the subjects, method returns "numberOfSubjects"
         calculateNumberOfSubjects(mav, allSubjects);
+
+        /** The following parts deal with exam information */
+
         return mav;
 
     }
@@ -332,5 +332,55 @@ public class semesterViewController {
             }
         }
         return sum;
+    }
+
+    /** Hilfsmethode 4 - Calculate List of Weekly Workload Values */
+    private List<Integer> workloadTotalWeekList (Long coreCourseId, LocalDate monday, List<LocalDate> allMondays){
+
+    // ... for all Mondays, call Hilfsmethode 5 and add to list.
+
+    /** Hilfsmethode 5 - Calculate Workload Value */
+    private int workloadTotalWeek(Long coreCourseId, LocalDate monday) {
+
+        Double[] workloadTotalDaysArray = workloadTotalDaysArray(coreCourseId, monday);
+
+        // this method is not finished, but the value will define the background color
+
+        return 0;
+    }
+
+    /** Hilfsmethode 6 - Calculate Workload Value */
+    private Double[] workloadTotalDaysArray(Long coreCourseId, LocalDate monday) {
+        List<Course> ccCourses = coreCourseRepo.findCoreCourseById(coreCourseId).getCourses();
+
+        List<Assignment> assignments = new ArrayList<>();
+
+        // add every exam for the week
+        ccCourses.stream().filter(c -> Objects.nonNull(c.getExams())).map(c -> c.getExams()).flatMap(List::stream)
+                .distinct().filter(e -> e.getDueDate().isAfter(monday.minusDays(1)))
+                .filter(e -> e.getDueDate().isBefore(monday.plusDays(7)))
+                .collect(Collectors.toCollection(() -> assignments));
+
+        // add every homework for the week
+        ccCourses.stream().filter(c -> Objects.nonNull(c.getHomeworks())).map(c -> c.getHomeworks())
+                .flatMap(List::stream).distinct().filter(e -> e.getDueDate().isAfter(monday.minusDays(1)))
+                .sorted((e1, e2) -> e1.getDueDate().compareTo(e2.getDueDate()))
+                .collect(Collectors.toCollection(() -> assignments));
+
+        assignments.stream().sorted((e1, e2) -> e1.getDueDate().compareTo(e2.getDueDate()))
+                .collect(Collectors.toList());
+
+        // Calculate Workload Value
+        Double[] workloadTotalDaysArray = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+
+        if (assignments != null) {
+            for (int i = 0; i < 7; i++) {
+
+                workloadTotalDaysArray[i] = Math.min(
+                        assignments.stream().map(c -> c.getWorkloadValue(monday.plusDays(i))).mapToDouble(w -> w).sum(),
+                        1);
+            }
+        }
+        return workloadTotalDaysArray;
     }
 }
