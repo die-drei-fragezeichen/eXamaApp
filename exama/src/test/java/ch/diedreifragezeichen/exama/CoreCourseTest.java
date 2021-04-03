9c20425446468585b63e023ed95646bb333366cb
BenedictGrupp <benedict.grupp@unifr.ch>
2021-04-03 20:53:23 +0200

    JUnitTest for CoreCourse (passed)

--- exama/src/test/java/ch/diedreifragezeichen/exama/CoreCourseTest.java
+++ exama/src/test/java/ch/diedreifragezeichen/exama/CoreCourseTest.java
@@ -1,21 +1,49 @@
 package ch.diedreifragezeichen.exama;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import ch.diedreifragezeichen.exama.courses.CoreCourse;
 import ch.diedreifragezeichen.exama.users.User;
 
 
 public class CoreCourseTest {
-    
+  
 @Test
-public void testAllMethods(){
+public void testGetSetId(){
     CoreCourse testCourse= new CoreCourse();
     testCourse.setId((long)1);
-    // testCourse.name="nametest";
-    // testCourse.enabled=true;
-    // testCourse.classTeacher=new User();
     assertEquals((long)1, testCourse.getId());
 }
 
+@Test
+public void testGetSetName(){
+    CoreCourse testCourse= new CoreCourse();
+    testCourse.setName("nametest");
+    assertEquals("nametest", testCourse.getName());
+}
+
+@Test
+public void testIsSetEnabled(){
+    CoreCourse testCourse= new CoreCourse();
+    testCourse.setEnabled(true);
+    assertEquals(true, testCourse.isEnabled());
+}
+
+@Test
+public void testGetSetClassTeacher(){
+    CoreCourse testCourse= new CoreCourse();
+    User user=new User();
+    testCourse.setClassTeacher(user);
+    assertEquals(user, testCourse.getClassTeacher());
+}
+
+@Test
+public void testGetSetStudents(){
+    CoreCourse testCourse= new CoreCourse();
+    ArrayList<User> liste=new ArrayList<User>();
+    testCourse.setStudents(liste);
+    assertEquals(liste, testCourse.getStudents());
+}
 }
